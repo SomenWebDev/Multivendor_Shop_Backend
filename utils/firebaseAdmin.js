05bb0438520b094b4d4b6a02b4994af337d8d6ae
SomenWebDev <somensingha18@gmail.com>
2025-06-30 20:31:03 +0600

    firebaseAdmin.js updated

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -3,6 +3,9 @@ const admin = require("firebase-admin");
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
 
+  // Replace escaped \n with actual newlines
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
